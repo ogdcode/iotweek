37a28361ff9cfd95007b4dcc4a27332cf372ec84
ogdsbucket <olivier.goncalves@hotmail.fr>
2017-04-04 13:10:10 +0200

    Add Exo 1.4 - BUTTON

---
+++ TP1/01-4_button/W1-4_BUTTON.ino
@@ -0,0 +1,21 @@
+const int BP = 3; // The pin where the button is connected to.
+
+// Initializaton function.
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {}
+  pinMode(BP, INPUT); // BP is an input (the button)
+}
+
+// Repeating main function.
+void loop() {
+  readBtn();
+  delay(1000);
+}
+
+void readBtn() {
+  int reading = digitalRead(BP);
+  Serial.print("reading = ");
+  Serial.println(reading);
+}
+

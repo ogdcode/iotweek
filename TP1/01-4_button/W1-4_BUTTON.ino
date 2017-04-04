a3335c9c36629e57615863394544b5770ec1486f
ogdsbucket <olivier.goncalves@hotmail.fr>
2017-04-04 14:39:21 +0200

    Update Exo 1.4 - BUTTON

--- TP1/01-4_button/W1-4_BUTTON.ino
+++ TP1/01-4_button/W1-4_BUTTON.ino
@@ -1,21 +1,39 @@
-const int BP = 3; // The pin where the button is connected to.
+const byte BTN = 2; // The pin where the button is connected to.
+volatile byte state = LOW;
 
 // Initializaton function.
 void setup() {
   Serial.begin(9600);
   while (!Serial) {}
-  pinMode(BP, INPUT); // BP is an input (the button)
+  pinMode(BTN, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  // Attach inpterrupt #0 (on pin 2) to the blink function, on state change
+  attachInterrupt(BTN, makeBlink, CHANGE);
 }
 
 // Repeating main function.
 void loop() {
-  readBtn();
-  delay(1000);
+  displaySerial("state = ", state);
+  digitalWrite(LED_BUILTIN, state);
 }
 
 void readBtn() {
-  int reading = digitalRead(BP);
-  Serial.print("reading = ");
-  Serial.println(reading);
+  int reading = digitalRead(BTN);
+  displaySerial("reading = ", reading);
 }
+
+void displaySerial(char* sentence, long value) {
+  Serial.print(sentence);
+  Serial.println(value);
+}
+
+void makeBlink() {
+  state = !state; // Switch variable state
+}
+
+void delaySeconds(int s) {
+  delay(1000 * s);
+}
+
+
 

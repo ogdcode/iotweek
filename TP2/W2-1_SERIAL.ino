56c0e2adc793a50ae38826d63b9da8bf2fadc60a
ogdsbucket <olivier.goncalves@hotmail.fr>
2017-04-04 11:06:43 +0200

    Add Exo 2.1 - SERIAL

---
+++ TP2/W2-1_SERIAL.ino
@@ -0,0 +1,35 @@
+unsigned long t;
+
+void setup() {
+  // Sets up the serial monitor
+  Serial.begin(9600);
+  while (!Serial) {
+  }
+
+  Serial.println("Init OK");
+}
+
+void loop() {
+  //simpleReadSerial();
+  timeMillis();
+}
+
+// Test serial communication
+void simpleReadSerial() {
+  if (Serial.available()) {
+    int lu = Serial.read();
+    Serial.println(lu);
+  } else {
+    Serial.println("Nothing...");
+  }
+  delay(2000);
+}
+
+void timeMillis() {
+  t = millis();
+  // Prints time since program started
+  Serial.print("$$> ");
+  Serial.println(t);
+  delay(500);  
+}
+

9881abb9bbee454585eabd0d93e9001a1c1762cc
ogdsbucket <olivier.goncalves@hotmail.fr>
2017-04-06 18:25:30 +0200

    Update Ruby scripts

--- Projet/api/arduino.rb
+++ Projet/api/arduino.rb
@@ -1,11 +1,10 @@
-#simplest ruby program to read from arduino serial,
-#using the SerialPort gem
-#(http://rubygems.org/gems/serialport)
+# Simple Ruby program to read from Arduino serial.
+# Uses the SerialPort gem (http://rubygems.org/gems/serialport)
 
 require "serialport"
 require_relative "server.rb"
 
-params for serial port
+# Parameters for serial port connection
 port_str = "#{ARGV[0]}"
 baud_rate = 9600
 data_bits = 8
@@ -14,7 +13,7 @@ parity = SerialPort::NONE
 
 sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
 
-#just read forever
+# Just read forever
 while true do
    while (data = sp.gets.chomp) do
       authorize(data)
